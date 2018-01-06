5d56c11d13ab9cdeb6f77bc0dd147741d100d6eb
Elf Sundae <elf.sundae@gmail.com>
2018-01-06 15:34:40 +0800

    Add http_client() helper function

---
+++ tests/HelpersTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use ElfSundae\HttpClient;
+use PHPUnit\Framework\TestCase;
+
+class HelpersTest extends TestCase
+{
+    public function test_http_client()
+    {
+        $this->assertInstanceOf(HttpClient::class, http_client());
+
+        $client = http_client('http://example.com');
+        $this->assertSame('http://example.com', (string) $client->getClient()->getConfig('base_uri'));
+
+        $client = http_client(['foo' => 'bar']);
+        $this->assertArraySubset(['foo' => 'bar'], $client->getClient()->getConfig());
+    }
+}
