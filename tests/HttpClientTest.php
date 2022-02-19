ac6cc274ae97e618e9ead3596bd944c0718b2c1e
Elf Sundae <elf.sundae@gmail.com>
2022-02-20 04:06:32 +0800

    tearDown(): void

--- tests/HttpClientTest.php
+++ tests/HttpClientTest.php
@@ -10,7 +10,7 @@ use PHPUnit\Framework\TestCase;
 
 class HttpClientTest extends TestCase
 {
-    protected function tearDown()
+    protected function tearDown(): void
     {
         HttpClient::setDefaultOptions([]);
         m::close();
